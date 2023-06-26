65356083a764b12c6dfb2a6c0398262b3aad2d9f
SandarvaShakya <sandarvashakya34@gmail.com>
2023-06-26 16:54:05 +0545

    ➕Add the remove tag feature

--- src/components/TagBar.jsx
+++ src/components/TagBar.jsx
@@ -4,11 +4,16 @@ import TagBox from './TagBox'
 
 const TagBar = () => {
   const { tags, setTags } = useContext(TagContext)
+
+  const handleClick = (clickedTag) => {
+    const removedTags = tags.filter(tag => clickedTag !== tag)
+    setTags(removedTags)
+  }
   
     return (
     <div className='tag-bar'>
         {tags.length > 0 && tags.map((tag, index) => {
-            return <TagBox  key={index} text={tag}/>
+            return <TagBox  key={index} text={tag} onClick={() => handleClick(tag)}/>
         })}
         {tags.length > 0 && <p className='clear' onClick={() => setTags([])}>Clear</p> }
     </div>
