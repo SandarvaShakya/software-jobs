e453a1cf3e6ad8c4b3fc0c5c1cccb0aebc319187
SandarvaShakya <sandarvashakya34@gmail.com>
2023-06-26 17:32:00 +0545

    ➕Add filtering feature

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios'
 import Card from './components/Card';
 import Header from './components/Header';
 import TagBar from './components/TagBar';
+import { TagContext } from './components/TagContext';
 
 const base_url = 'https://storage.googleapis.com/programiz-static/hiring/software/job-listing-page-challenge/data.json'
 
 const App = () => {
   //contains all the data
   const [jobData, setJobData] = useState([])
+  //contains filtered data
+  const [filteredJobData, setFilteredJobData] = useState([]);
+  const { tags } = useContext(TagContext)
 
   //fetch data
   useEffect(() => {
@@ -19,17 +23,27 @@ const App = () => {
     })
   }, [])
 
+  //filter data based on the tags
+  useEffect(() => {
+    if (tags.length === 0) {
+      // If no tags are selected, show all jobs
+      setFilteredJobData(jobData);
+    } else {
+      const filteredJobs = jobData.filter(job => {
+        return tags.every(tag => job.keywords.includes(tag));
+      });
+      setFilteredJobData(filteredJobs);
+    }
+  }, [tags, jobData]);
+
   return (
     <main>
       <Header />
       <TagBar />
       <div className="cards">
-        {jobData.map((job, index) => {
-          return <Card
-            key={index}
-            job={job}
-          />
-        })}
+          {filteredJobData.map((job, index) => (
+              <Card key={index} job={job} />
+          ))}
       </div>
     </main>
   )
